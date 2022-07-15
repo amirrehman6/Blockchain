3fddfa40301fc72ace63b8dd102e3e23b5ba11d3
amirrehman6 <amir.rehman@yahoo.com>
2022-07-16 00:16:05 +0500

    3rd upload

---
+++ 5-manipulateStrings/findingSegment.js
@@ -0,0 +1,3 @@
+var textPara = "It is startling to think that, even in the darkest depths of World War II, J. R. R. Tolkien was writing the trilogy, which contains, with the weird applicability available only to poetry and myth, the essential notion that the good gray wizard can understand the evil magi precisely because he is just enough like them to grasp their minds and motives in ways that they cannot grasp his."
+var firstChar = textPara.indexOf("World War II");
+document.write(firstChar);
---
+++ 7-dateTime/date.js
@@ -0,0 +1,11 @@
+// displaying date
+var rightNow = new Date();
+document.write(rightNow);
+
+//Printing day from the Date() object
+var now = new Date();
+var theDay = now.getDay();
+
+var daysStr = ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'];
+var dayString = daysStr(theDay);
+
