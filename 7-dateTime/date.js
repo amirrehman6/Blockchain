b2b2dcab6b9003eeb18c9e42680a9457f4782545
amirrehman6 <amir.rehman@yahoo.com>
2022-07-16 00:39:56 +0500

    3rd upload

--- 7-dateTime/date.js
+++ 7-dateTime/date.js
@@ -5,10 +5,22 @@ var rightNow = new Date();
 document.write(rightNow);
 document.write("<br>");
 
-//Printing day from the Date() object
+//printing day from the Date() object
 var daysStr = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
 var now = new Date();
 var theDay = now.getDay();
 var dayName = daysStr[theDay];
 
 document.write(theDay +"'s day - "+ dayName);
+
+document.write("<br>");
+
+//printing other date methods
+document.write(now.getMonth() + "<br>");
+document.write(now.getDate() + "<br>");
+document.write(now.getFullYear() + "<br>");
+document.write(now.getHours() + "<br>");
+document.write(now.getMinutes() + "<br>");
+document.write(now.getSeconds() + "<br>");
+document.write(now.getMilliseconds() + "<br>");
+document.write(now.getTime() + "<br>");
